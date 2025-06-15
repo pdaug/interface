9c7e7933fdccaab00c9031f149862719c254e4d2
pdaug <dev.pdaug@gmail.com>
2025-06-15 14:55:16 -0300

    update: deploy storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,7 +14,11 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {
+      viteConfig: {
+        base: "/components/",
+      },
+    },
   },
 };
 export default config;
