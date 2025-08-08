3e3d03652c7dece48dd188989b19ad19dd8a9a60
pdaug <dev.pdaug@gmail.com>
2025-08-08 19:10:09 -0300

    fix: audio import in network always actions in browser

--- src/hooks/useSounds.tsx
+++ src/hooks/useSounds.tsx
@@ -1,28 +1,13 @@
-import { useRef } from "react";
+const sources = {
+  ok: "/sounds/ok.mp3",
+  alert: "/sounds/alert.mp3",
+  login: "/sounds/login.mp3",
+  logout: "/sounds/logout.mp3",
+};
 
 const useSounds = function () {
-  const sources = {
-    ok: "/sounds/ok.mp3",
-    alert: "/sounds/alert.mp3",
-    login: "/sounds/login.mp3",
-    logout: "/sounds/logout.mp3",
-  };
-
-  type TypeSource = keyof typeof sources;
-
-  const audioRefs = new Object() as Record<
-    TypeSource,
-    React.RefObject<HTMLAudioElement>
-  >;
-
-  for (const type of Object.keys(sources)) {
-    const source = sources[type as TypeSource];
-    const audioRef = useRef(new Audio(source));
-    audioRefs[type as TypeSource] = audioRef;
-  }
-
   const play = async function (type: keyof typeof sources) {
-    const audio = audioRefs[type]?.current;
+    const audio = new Audio(sources[type]);
     if (!audio) return;
     audio.volume = 0.3;
     audio.currentTime = 0;
