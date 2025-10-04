033b8268bdf5f9f97d24c0eeb4e654ae5e10ab3d
pdaug <dev.pdaug@gmail.com>
2025-10-04 11:53:26 -0300

    update: vehicle list display maintenance

--- src/pages/services/vehicles/VehiclesList.tsx
+++ src/pages/services/vehicles/VehiclesList.tsx
@@ -12,7 +12,7 @@ import { toast } from "sonner";
 import React, { useState } from "react";
 import { useDebounce } from "use-debounce";
 import { useNavigate } from "react-router-dom";
-import { endOfDay, startOfYear } from "date-fns";
+import { endOfDay, isSameDay, startOfYear } from "date-fns";
 
 //apis
 import apis from "../../../apis";
@@ -28,7 +28,11 @@ import Download from "../../../utils/Download";
 import Clipboard from "../../../utils/Clipboard";
 
 // types
-import { TypeVehicle, TypeVehicleRefuel } from "../../../types/Vehicle";
+import {
+  TypeVehicle,
+  TypeVehicleMaintenance,
+  TypeVehicleRefuel,
+} from "../../../types/Vehicle";
 import { ApiResponsePaginate } from "../../../types/Api";
 import { TypeInputInterval } from "../../../types/Components";
 
@@ -61,8 +65,8 @@ const VehiclesList = function () {
   const navigate = useNavigate();
   const Currency = useCurrency();
   const { renderByPlan } = usePermission();
-  const { instanceDateTime } = useDateTime();
   const { OpenDialog, CloseDialog } = useDialog();
+  const { instanceDate, instanceDateTime } = useDateTime();
   const { users, token, instance, workspaces, workspaceId } = useSystem();
 
   const [vehicles, setVehicles] = useState<TypeVehicle[]>([]);
@@ -73,6 +77,12 @@ const VehiclesList = function () {
   const [refuels, setRefuels] = useState<TypeVehicleRefuel[]>([]);
   const [pageRefuels, setPageRefuels] = useState<number>(1);
   const [totalRefuels, setTotalRefuels] = useState<number>(0);
+
+  const [maintenances, setMaintenances] = useState<TypeVehicleMaintenance[]>(
+    [],
+  );
+  const [pageMaintenances, setPageMaintenances] = useState<number>(1);
+  const [totalMaintenances, setTotalMaintenances] = useState<number>(0);
 
   const [search, setSearch] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
@@ -155,7 +165,6 @@ const VehiclesList = function () {
         toast.warning(t.toast.warning_error, {
           description: t.stacks.no_find_item,
         });
-        navigate("/f/vehicles");
         setLoading(false);
         return;
       }
@@ -167,7 +176,44 @@ const VehiclesList = function () {
         description: t.stacks.no_find_item,
       });
       console.error("[src/pages/services/vehicles/VehiclesList.tsx]", err);
-      navigate("/f/vehicles");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const FetchMaintenance = async function () {
+    setLoading(true);
+    try {
+      const responseMaintenance = await apis.VehicleMaintenance.list<
+        ApiResponsePaginate<TypeVehicleMaintenance>
+      >(
+        token,
+        instance.name,
+        {
+          pageSize,
+          pageCurrent: searchDebounced ? 1 : pageMaintenances,
+          searchField: "name",
+          search: searchDebounced,
+        },
+        workspaceId,
+      );
+      if (
+        !responseMaintenance.data?.result?.items ||
+        responseMaintenance.status !== 200
+      ) {
+        play("alert");
+        toast.warning(t.toast.warning_error, {
+          description: t.stacks.no_find_item,
+        });
+      }
+      setMaintenances(responseMaintenance.data.result.items);
+      setTotalMaintenances(responseMaintenance.data.result.pagination.total);
+    } catch (err) {
+      play("alert");
+      toast.error(t.toast.warning_error, {
+        description: t.stacks.no_find_item,
+      });
+      console.error("[src/pages/services/vehicles/VehiclesList.tsx]", err);
     } finally {
       setLoading(false);
     }
@@ -183,6 +229,14 @@ const VehiclesList = function () {
 
   // fetch refuel
   useAsync(FetchRefuel, [interval, workspaceId, pageRefuels, searchDebounced]);
+
+  // fetch maintenance
+  useAsync(FetchMaintenance, [
+    interval,
+    workspaceId,
+    pageMaintenances,
+    searchDebounced,
+  ]);
 
   const getOptionsVehicles = [
     {
@@ -348,6 +402,70 @@ const VehiclesList = function () {
     },
   ];
 
+  const getOptionsMaintenance = [
+    {
+      id: "edit",
+      label: t.components.edit,
+      Icon: PencilSimple,
+      onClick: function (_: React.MouseEvent, data: unknown) {
+        if (data && typeof data === "object" && "id" in data) {
+          navigate(`/f/vehicles/maintenance/${data.id}`);
+        }
+        return;
+      },
+    },
+    {
+      id: "delete",
+      label: t.components.delete,
+      Icon: Trash,
+      IconColor: "var(--dangerColor",
+      styles: { color: "var(--dangerColor)" },
+      onClick: async function (_: React.MouseEvent, data: unknown) {
+        if (!data || typeof data !== "object" || !("id" in data)) return;
+        OpenDialog({
+          category: "Danger",
+          title: t.dialog.title_delete,
+          description: t.dialog.description_delete,
+          confirmText: t.components.delete,
+          onConfirm: async function () {
+            try {
+              const response = await apis.VehicleMaintenance.delete(
+                token,
+                instance.name,
+                data.id as string,
+                workspaceId,
+              );
+              if (!response.data?.result) {
+                play("alert");
+                toast.warning(t.toast.warning_error, {
+                  description: t.toast.error_delete,
+                });
+                return;
+              }
+              play("ok");
+              toast.success(t.toast.success, {
+                description: t.toast.success_delete,
+              });
+              CloseDialog();
+              await FetchMaintenance();
+            } catch (err) {
+              play("alert");
+              toast.error(t.toast.warning_error, {
+                description: t.toast.error_delete,
+              });
+              console.error(
+                "[src/pages/services/vehicles/VehiclesList.tsx]",
+                err,
+              );
+            }
+            return;
+          },
+        });
+        return;
+      },
+    },
+  ];
+
   return renderByPlan(
     "advanced",
     <React.Fragment>
@@ -459,7 +577,7 @@ const VehiclesList = function () {
         </Tooltip>
       </Horizontal>
 
-      <Vertical internal={1}>
+      <Vertical internal={1} styles={{ marginBottom: "1rem" }}>
         <Table
           border
           loading={loading}
@@ -558,20 +676,13 @@ const VehiclesList = function () {
                 );
               },
             },
-            description: {
-              label: t.components.description,
-              handler: function (data) {
-                if (data.description) return data.description as string;
-                return (
-                  <i style={{ color: "var(--textLight)" }}>
-                    {t.stacks.no_description}
-                  </i>
-                );
-              },
+            plate: {
+              label: t.vehicle.plate,
+              maxWidth: 128,
             },
-            plate: { label: t.vehicle.plate },
             brand: {
               label: t.vehicle.brand,
+              maxWidth: 200,
               handler: function (data) {
                 const brand = (data?.brand || "") as string;
                 const brandFinded = VehicleBrandsOptions?.find(
@@ -595,23 +706,22 @@ const VehiclesList = function () {
                         </i>
                       )
                     }
+                    description={
+                      data.model ? (
+                        (data.model as string)
+                      ) : (
+                        <i style={{ color: "var(--textLight)" }}>
+                          {t.vehicle.no_model}
+                        </i>
+                      )
+                    }
                   />
-                );
-              },
-            },
-            model: {
-              label: t.vehicle.model,
-              handler: function (data) {
-                if (data.model) return data.model as string;
-                return (
-                  <i style={{ color: "var(--textLight)" }}>
-                    {t.vehicle.no_model}
-                  </i>
                 );
               },
             },
             user: {
               label: t.components.user,
+              maxWidth: 200,
               handler: function (data) {
                 const userFinded = users?.find(function (user) {
                   return user.id === data.userId;
@@ -635,6 +745,7 @@ const VehiclesList = function () {
             },
             createdAt: {
               label: t.components.created_at,
+              maxWidth: 180,
               handler: function (data) {
                 const datetime = instanceDateTime(data.createdAt as string);
                 return datetime;
@@ -652,7 +763,9 @@ const VehiclesList = function () {
         />
       </Vertical>
 
-      <Vertical internal={1}>
+      <Vertical internal={1} styles={{ marginBottom: "1rem" }}>
+        <h2>{t.vehicle.refuel}</h2>
+
         <Table
           border
           noSelect
@@ -662,7 +775,7 @@ const VehiclesList = function () {
           columns={{
             fuel: {
               label: t.vehicle.fuel,
-              maxWidth: "160px",
+              maxWidth: 208,
               handler: function (data) {
                 return (
                   <Badge
@@ -717,6 +830,7 @@ const VehiclesList = function () {
             },
             vehicleId: {
               label: t.vehicle.vehicle,
+              maxWidth: 200,
               handler: function (data) {
                 const brandFinded = VehicleBrandsOptions?.find(
                   function (brand) {
@@ -756,7 +870,7 @@ const VehiclesList = function () {
             },
             user: {
               label: t.components.user,
-              maxWidth: 180,
+              maxWidth: 200,
               handler: function (data) {
                 const userFinded = users?.find(function (user) {
                   return user.id === data.userId;
@@ -797,6 +911,163 @@ const VehiclesList = function () {
           pageSize={pageSize}
         />
       </Vertical>
+
+      <Vertical internal={1} styles={{ marginBottom: "1rem" }}>
+        <h2>{t.vehicle.maintenance}</h2>
+
+        <Table
+          border
+          noSelect
+          loading={loading}
+          options={getOptionsMaintenance}
+          data={maintenances as TableData[]}
+          columns={{
+            type: {
+              label: t.vehicle.type,
+              maxWidth: 96,
+              handler: function (data) {
+                return (
+                  <Badge
+                    category="Neutral"
+                    value={
+                      t.vehicle[data.type as keyof typeof t.vehicle] ||
+                      t.components.unknown
+                    }
+                  />
+                );
+              },
+            },
+            category: {
+              label: t.vehicle.category,
+              maxWidth: 96,
+              handler: function (data) {
+                return (
+                  <Badge
+                    category="Neutral"
+                    value={
+                      t.vehicle[data.category as keyof typeof t.vehicle] ||
+                      t.components.unknown
+                    }
+                  />
+                );
+              },
+            },
+            name: {
+              label: t.vehicle.maintenance,
+              handler: function (data) {
+                return (
+                  <div
+                    className="cursor"
+                    onClick={() =>
+                      navigate(`/f/vehicles/maintenance/${data.id}`)
+                    }
+                  >
+                    {data.name as string}
+                  </div>
+                );
+              },
+            },
+            vehicleId: {
+              label: t.vehicle.vehicle,
+              maxWidth: 200,
+              handler: function (data) {
+                const brandFinded = VehicleBrandsOptions?.find(
+                  function (brand) {
+                    return (
+                      brand.name ===
+                      ((data?.vehicleBrand as string) || "")?.toLowerCase()
+                    );
+                  },
+                );
+                return (
+                  <Profile
+                    padding={false}
+                    photoIcon={CarProfile}
+                    name={data?.vehicleName as string}
+                    photo={brandFinded?.image || ""}
+                    description={
+                      brandFinded?.name?.toLowerCase() === "other" ? (
+                        t.components.other
+                      ) : brandFinded?.name ? (
+                        `${brandFinded?.name?.slice(0, 1).toUpperCase()}${brandFinded?.name?.slice(1)}`
+                      ) : (
+                        <i style={{ color: "var(--textLight)" }}>
+                          {t.vehicle.no_brand}
+                        </i>
+                      )
+                    }
+                  />
+                );
+              },
+            },
+            pricingTotal: {
+              label: t.components.total,
+              maxWidth: 160,
+              handler: function (data) {
+                return <span>{Currency(data.pricingTotal as string)}</span>;
+              },
+            },
+            user: {
+              label: t.components.user,
+              maxWidth: 200,
+              handler: function (data) {
+                const userFinded = users?.find(function (user) {
+                  return user.id === data.userId;
+                });
+                return (
+                  <Tooltip
+                    content={t.components[userFinded?.role || "collaborator"]}
+                  >
+                    <Profile
+                      photoCircle
+                      photoSize={3}
+                      padding={false}
+                      styles={{ lineHeight: 1 }}
+                      photo={userFinded?.photo || ""}
+                      description={userFinded?.email || ""}
+                      name={userFinded?.name || t.components.unknown}
+                    />
+                  </Tooltip>
+                );
+              },
+            },
+            maintenanceStart: {
+              label: t.vehicle.maintenance_start,
+              maxWidth: 180,
+              handler: function (data) {
+                const dateEnd =
+                  data.maintenanceEstimatedEnd || data.maintenanceEnd;
+
+                const datetimeStart = instanceDate(
+                  data.maintenanceStart as string,
+                );
+                const datetimeEnd = instanceDate(dateEnd as string);
+
+                const startIsSameDayEnd = isSameDay(
+                  data.maintenanceStart as string,
+                  dateEnd as string,
+                );
+
+                return startIsSameDayEnd
+                  ? datetimeStart
+                  : `${datetimeStart} - ${datetimeEnd}`;
+              },
+            },
+          }}
+        />
+
+        <Pagination
+          display
+          pageCurrent={pageMaintenances}
+          setPage={setPageMaintenances}
+          itemsTotal={totalMaintenances}
+          pageSize={pageSize}
+        />
+      </Vertical>
+
+      <div>
+        <div style={{ height: 128 }}></div>
+      </div>
     </React.Fragment>,
   );
 };
